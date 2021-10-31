75e79c1148e8e3785e1fd7a8ad8d93cc189ec735
furkan <ozmenf97@gmail.com>
2021-10-31 19:13:00 +0300

    AuthorController.java update

--- src/main/java/com/example/myblog/controller/AuthorController.java
+++ src/main/java/com/example/myblog/controller/AuthorController.java
@@ -1,12 +1,15 @@
 package com.example.myblog.controller;
 
-import com.example.myblog.core.ApiResponseHandler;
+import com.example.myblog.dto.ResponseApi;
 import com.example.myblog.dto.request.AuthorSaveRequest;
 import com.example.myblog.service.AuthorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.transaction.Transactional;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
 
 @RestController
 @RequestMapping("/api/v1/authors")
@@ -21,20 +24,28 @@ public class AuthorController {
     @Transactional
     @PostMapping("/")
     public ResponseEntity<?> createAuthor(@RequestBody AuthorSaveRequest authorSaveRequest){
-        return ApiResponseHandler.jsonGenerateResponse(
-                "created Author",
-                HttpStatus.CREATED,
-                authorService.createAuthor(authorSaveRequest)
+        return ResponseEntity.ok(
+                new ResponseApi.ResponseBuilder()
+                        .TimeStamp(now())
+                        .Message("created author")
+                        .StatusCode(201)
+                        .HttpStatus(HttpStatus.CREATED)
+                        .Data(Map.of("blogs", authorService.createAuthor(authorSaveRequest)))
+                        .build()
         );
     }
 
     @Transactional
     @GetMapping("/get/authors")
     public ResponseEntity<?> listAuthor(){
-        return ApiResponseHandler.jsonGenerateResponse(
-                "listed Authors",
-                HttpStatus.OK,
-                authorService.authorList()
+        return ResponseEntity.ok(
+                new ResponseApi.ResponseBuilder()
+                        .TimeStamp(now())
+                        .Message("created blog")
+                        .StatusCode(201)
+                        .HttpStatus(HttpStatus.CREATED)
+                        .Data(Map.of("blogs", authorService.authorList()))
+                        .build()
         );
     }
 }
