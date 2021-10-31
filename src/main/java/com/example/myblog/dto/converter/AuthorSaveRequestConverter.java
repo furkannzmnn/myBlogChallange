5320b1db323afa9a75c86422d2404d00ef6d5b5b
furkan <ozmenf97@gmail.com>
2021-10-31 19:13:21 +0300

    initial message

--- src/main/java/com/example/myblog/dto/converter/AuthorSaveRequestConverter.java
+++ src/main/java/com/example/myblog/dto/converter/AuthorSaveRequestConverter.java
@@ -4,10 +4,11 @@ import com.example.myblog.dto.request.AuthorSaveRequest;
 import com.example.myblog.model.Author;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class AuthorSaveRequestConverter {
 
     public Author authorSaveRequestToAuthor(AuthorSaveRequest request){
         return new Author(request.getAuthorFullName());
     }
-}
+    }
