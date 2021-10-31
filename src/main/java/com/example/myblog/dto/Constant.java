aad40e7995342e08b61e734b962805ea682424d0
furkan <ozmenf97@gmail.com>
2021-10-31 19:12:12 +0300

    Constant enum class added

---
+++ src/main/java/com/example/myblog/dto/Constant.java
@@ -0,0 +1,13 @@
+package com.example.myblog.dto;
+
+public enum Constant {
+    // FOUND MESSAGE
+    BLOG_NOT_FOUND,
+    AUTHOR_NOT_FOUND,
+
+    // CREATED MESSAGE
+    BLOG_IS_NOT_CREATED,
+    AUTHOR_IS_NOT_CREATED
+
+
+}
